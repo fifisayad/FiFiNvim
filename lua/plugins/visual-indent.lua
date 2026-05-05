c08700122d9be0a4cb99805d2925e9fbd5029c60
fifisayad <sayad.mehrdad@gmail.com>
2026-05-05 14:47:14 +0330

    feat: add my mapping for changing indents

---
+++ lua/plugins/visual-indent.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "AstroNvim/astrocore",
+    opts = {
+      mappings = {
+        v = {
+          [">"] = { ">gv", desc = "Indent right and reselect" },
+          ["<"] = { "<gv", desc = "Indent left and reselect" },
+        },
+        x = {
+          [">"] = { ">gv", desc = "Indent right and reselect" },
+          ["<"] = { "<gv", desc = "Indent left and reselect" },
+        },
+      },
+    },
+  },
+}
