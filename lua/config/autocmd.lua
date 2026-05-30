7b66b00eb2e99b96a39c88863065ed40c7ef79d3
fifisayad <sayad.mehrdad@gmail.com>
2026-05-30 20:31:07 +0330

    fix: issue with asynchronous execution of script

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -1,14 +1,11 @@
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "python",
   callback = function(event)
-    vim.keymap.set("n", "<leader>w", function()
-      vim.cmd.write()
-      vim.cmd("PyrightOrganizeImports")
-      vim.cmd.write()
-    end, {
+    vim.api.nvim_create_autocmd("BufWritePre", {
       buffer = event.buf,
-      desc = "Save and run PyrightOrganizeImports",
+      callback = function()
+        vim.cmd("PyrightOrganizeImports")
+      end,
     })
   end,
 })
-
