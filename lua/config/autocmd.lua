f69ebb7866d793a7e52ff57e32247e8b7231630a
fifisayad <sayad.mehrdad@gmail.com>
2026-05-30 16:19:44 +0330

    fix: first organize python import and then write buffer

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -2,8 +2,8 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = "python",
   callback = function(event)
     vim.keymap.set("n", "<leader>w", function()
-      vim.cmd.write()
       vim.cmd("PyrightOrganizeImports")
+      vim.cmd.write()
     end, {
       buffer = event.buf,
       desc = "Save and run PyrightOrganizeImports",
