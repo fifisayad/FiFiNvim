6c72363eb15f15f6b947b8bad1f316a33fb54c7c
fifisayad <sayad.mehrdad@gmail.com>
2026-05-31 22:23:37 +0330

    refactor: for fzf telescope plugin

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,14 +1,23 @@
-return {{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+return {
+  {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = "make",
+  },
+
   {
     "nvim-telescope/telescope.nvim",
     opts = function(_, opts)
       opts.extensions = opts.extensions or {}
+
       opts.extensions.fzf = {
         fuzzy = true,
         override_generic_sorter = true,
         override_file_sorter = true,
         case_mode = "smart_case",
       }
+
+      local telescope = require("telescope")
+      telescope.load_extension("fzf")
     end,
   },
 }
