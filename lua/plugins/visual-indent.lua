fa27bb4118240b531ab258d7a35736a7b0133658
fifisayad <sayad.mehrdad@gmail.com>
2026-06-04 11:14:54 +0330

    feat: wrap paranteses in visual mode

--- lua/plugins/visual-indent.lua
+++ lua/plugins/visual-indent.lua
@@ -6,10 +6,12 @@ return {
         v = {
           [">"] = { ">gv", desc = "Indent right and reselect" },
           ["<"] = { "<gv", desc = "Indent left and reselect" },
+          ["("] = { "c()<Esc>hp", desc = "Wrap selection in parentheses" }
         },
         x = {
           [">"] = { ">gv", desc = "Indent right and reselect" },
           ["<"] = { "<gv", desc = "Indent left and reselect" },
+          ["("] = { "c()<Esc>hp", desc = "Wrap selection in parentheses" },
         },
       },
     },
