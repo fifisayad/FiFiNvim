70c902a84a0aaf0bd48a5ad7b5acfda2d5d20bff
fifisayad <sayad.mehrdad@gmail.com>
2026-06-05 16:17:31 +0330

    feat: enable bash lsp

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroLSP allows you to customize the features in AstroNvim's LSP configuration engine
 -- Configuration documentation can be found with `:h astrolsp`
@@ -22,7 +22,7 @@ return {
       format_on_save = {
         enabled = true, -- enable or disable format on save globally
         allow_filetypes = { -- enable format on save for specified filetypes only
-          -- "go",
+          -- "go",   
         },
         ignore_filetypes = { -- disable format on save for specified filetypes
           -- "python",
@@ -44,7 +44,9 @@ return {
     -- customize language server configuration passed to `vim.lsp.config`
     -- client specific configuration can also go in `lsp/` in your configuration root (see `:h lsp-config`)
     config = {
-      -- ["*"] = { capabilities = {} }, -- modify default LSP client settings such as capabilities
+      bashls = {
+        filetypes = { "sh", "bash", "zsh" },  -- add zsh here
+      },
     },
     -- customize how language servers are attached
     handlers = {
